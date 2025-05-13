f13946beb8259e2175d94e59f3b0b06a1af789b1
kami-christella <kamichristella@gmail.com>
2025-05-13 12:24:23 +0200

    added loops

--- loops.ts
+++ loops.ts
@@ -9,3 +9,9 @@ console.log(candidates.length);
 for (let i:number=candidates.length-1;i>=0; i--){
     console.log(candidates[i])
 }
+
+const numbersList: number []=[1,2,3,4,5,6,7,8,9,10]
+
+for(let n:number=0;n<numbersList.length; n++){
+    console.log(numbersList[n])
+}
