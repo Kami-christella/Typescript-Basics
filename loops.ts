2917b5194c8af481d6e741c0427610056d77355b
kami-christella <kamichristella@gmail.com>
2025-05-13 12:53:20 +0200

    added while loop

--- loops.ts
+++ loops.ts
@@ -1,17 +1,32 @@
-const candidates: string [] =[];
-candidates.push('Iradukunda')
-candidates.push('Aline')
-candidates.push('Placide')
-candidates.push('Peter');
+// const candidates: string [] =[];
+// candidates.push('Iradukunda')
+// candidates.push('Aline')
+// candidates.push('Placide')
+// candidates.push('Peter');
 
-console.log(candidates.length);
+// console.log(candidates.length);
 
-for (let i:number=candidates.length-1;i>=0; i--){
-    console.log(candidates[i])
-}
+// for (let i:number=candidates.length-1;i>=0; i--){
+//     // candidates.push('ooo');
+//     console.log(candidates[i])
+// }
 
-const numbersList: number []=[1,2,3,4,5,6,7,8,9,10]
+// const numbersList: number []=[1,2,3,4,5,6,7,8,9,10]
 
-for(let n:number=0;n<numbersList.length; n++){
-    console.log(numbersList[n])
+// for(let n:number=0;n<numbersList.length; n++){
+//     console.log(numbersList[n])
+// }
+
+// //Another loop where you don't write a condition
+
+// for(const candidate of candidates){
+//     console.log(candidate)
+// }
+
+// while loop
+
+let i=0;
+while(i<=5){
+    console.log(i);
+    i++;
 }
