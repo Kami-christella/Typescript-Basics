9d98fcade630c68a6cd75ef96a17ed1a39168c55
kami-christella <kamichristella@gmail.com>
2025-05-13 12:17:46 +0200

    created loops.ts file

---
+++ loops.ts
@@ -0,0 +1,11 @@
+const candidates: string [] =[];
+candidates.push('Iradukunda')
+candidates.push('Aline')
+candidates.push('Placide')
+candidates.push('Peter');
+
+console.log(candidates.length);
+
+for (let i:number=candidates.length-1;i>=0; i--){
+    console.log(candidates[i])
+}
